69c30bd77477525b04fe91bb4e18e6efa75c5381
2kjin <lkjin1202@gmail.com>
2023-04-06 23:22:47 +0900

    update: boj_silver4_1940_주몽

---
+++ baekjoon/silver4_1940_주몽/1940.py
@@ -0,0 +1,19 @@
+n = int(input())
+m = int(input())
+lst = list(map(int,input().split()))
+lst.sort()
+
+cnt = 0
+i = 0
+j = n-1
+
+while i < j:
+    if lst[i] + lst[j] == m:
+        cnt +=1
+        i += 1
+        j -= 1
+    elif lst[i] + lst[j] < m:
+        i += 1
+    else:
+        j -= 1
+print(cnt)
